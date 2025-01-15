df4f5ed062b07233ff919f5375eed957f048f029
Nishanthi-Git <veerasamynishanthi@gmail.com>
2025-01-15 17:13:44 +0900

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Nishanthi</h1>
+        <p className={styles.description}>
+        I’m a budding frontend developer with a strong foundation in HTML, CSS, and JavaScript, and I’m exploring React to build dynamic user interfaces. Excited to learn, grow, and contribute—let’s connect!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
